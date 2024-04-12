b3a7af7bcef191b7ff82956116a8cf765ab944aa
Tamar Bem Nahum <105340024+TamarBeNahum@users.noreply.github.com>
2024-04-12 15:34:34 +0300

    2

--- src/FFCompos/TableFullSyllabus.jsx
+++ src/FFCompos/TableFullSyllabus.jsx
@@ -1,22 +1,26 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import MenuLogo from '../FFCompos/MenuLogo';
+import { Grid, Typography, Button, Paper, Container, Box} from '@mui/material';
+
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'id', headerName: 'ID',flex: 0.5, minWidth: 70 },
+  { field: 'firstName', headerName: 'First name',flex: 1, minWidth: 130 },
+  { field: 'lastName', headerName: 'Last name',flex: 1,  minWidth: 130 },
   {
     field: 'age',
     headerName: 'Age',
     type: 'number',
-    width: 90,
+    flex: 0.5, 
+    minWidth: 90,
   },
   {
     field: 'fullName',
     headerName: 'Full name',
     description: 'This column has a value getter and is not sortable.',
     sortable: false,
-    width: 160,
+    minWidth: 160,
+     flex: 1.5,
     valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
   },
 ];
@@ -33,24 +37,37 @@ const rows = [
   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
-export default function DataTable() {
+export default function TableFullSyllabus() {
   return (
     <>
-<MenuLogo />
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
-          },
-        }}
-        pageSizeOptions={[5, 10]}
-        checkboxSelection
-      />
-    </div>
+      <MenuLogo />
+      <Container sx={{ maxWidth: '100%', mb: 3, mt: 8 }} >
+        <Grid container spacing={2} alignItems="center">
+
+            <Paper sx={{
+            p: 2, m: 2, backgroundColor: '#FFFAFA', display: 'inline-block',
+            width: '100%',
+          }}>
+            <Box sx={{ width: '100%', height: 400 }}>
+              <DataGrid
+              rows={rows}
+              columns={columns}
+              initialState={{
+                pagination: {
+                  paginationModel: { page: 0, pageSize: 5 },
+                },
+              }}
+              pageSizeOptions={[5, 10]}
+              checkboxSelection
+            />
+            </Box>
+         
+          </Paper>
+        
+        </Grid>
+      </Container>
+
     </>
-    
+
   );
 }
