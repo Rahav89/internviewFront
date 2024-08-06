d28bc542019b22c974ce51114b4f5f317ba37fd9
Tamar Bem Nahum <105340024+TamarBeNahum@users.noreply.github.com>
2024-08-06 10:51:08 +0300

    הסבר לאלגו

--- src/routingForAlgo/SurgerySchedule.jsx
+++ src/routingForAlgo/SurgerySchedule.jsx
@@ -146,6 +146,21 @@ export default function SurgerySchedule() {
           direction: "rtl",
         }}
       >
+        <Typography sx={{ textAlign: "right", flexGrow: 1, fontSize: 17 }}>
+          לפניך לוח שנה המציג את כלל הניתוחים במערך הכירורגיה.
+          <br />
+          כדי לחשב שיבוץ מתמחים לניתוחים, בחר שבוע רלוונטי על ידי לחיצה וגרירה בלוח השנה    
+          ,ולאחר מכן לחץ על ←
+          <Button
+            variant="outlined"
+            color="secondary"
+            sx={{ fontSize: 12, mr: 0.7,  padding: 0.5 , mt: { xs: 0.5, sm: -0.5 }}}
+          >
+            חשב אלגוריתם שיבוץ
+          </Button>
+        </Typography>
+
+
         <Box
           sx={{
             display: "flex",
@@ -155,6 +170,7 @@ export default function SurgerySchedule() {
             direction: "ltr",
           }}
         >
+
           <Typography variant="h4" sx={{ textAlign: "left", flexGrow: 1 }}>
             {currentMonth.format("MMMM YYYY")}
           </Typography>
